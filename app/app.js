9cbcd9593a98b70183312b273300ffd13be1d215
TRISHAN00 <trishansaha43@gmail.com>
2023-12-01 21:30:44 +0600

    final setup

--- app/app.js
+++ app/app.js
@@ -1,27 +1,14 @@
 require("dotenv").config("../.env");
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const middleware = require("./middleware");
+const { errorHandler, notFoundHandler } = require("./error");
+const router = require("./routes");
 
 const app = express();
 
-app.use([morgan("dev"), cors(), express.json()]);
-
-app.get("/health", (_req, res) => {
-  res.status(200).json({ message: "Success" });
-});
-
-app.use((_req, _res, next) => {
-  const error = new Error("Resouce not found..");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, _req, res, _next) => {
-  if (error.status) {
-    return res.status(error.status).json({ message: error.message });
-  }
-  res.status(500).json({ message: "Something went wrong" });
-});
+app.use(middleware);
+app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
